f4e2846c4f69f039c25f333b47b5f4e493967a6c
Jushkin <jushkin.saparmatov@gmail.com>
2023-04-20 16:56:56 +0500

    rename fs solved

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,8 @@
+import path from 'path'
+import fs from 'fs'
+
 const copy = async () => {
-    // Write your code here 
-};
+      
+    }
 
 await copy();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,33 @@
+import { log, error as errorLog } from 'console';
+import { rename as renameFile, existsSync } from 'fs'
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'node:url';
+
 const rename = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const wrongFilePath = join(__dirname, 'files', 'wrongFilename.txt')
+    const newFilePath = join(__dirname, 'files', 'properFilename.md')
+
+    log(wrongFilePath, newFilePath)
+    try {
+        const srcExists = existsSync(wrongFilePath);
+        if (!srcExists) {
+            throw new Error('FS operation failed')
+        }
+        const dest = existsSync(newFilePath);
+        if (dest) {
+            throw new Error('FS operation failed')
+        }
+
+        renameFile('./files/wrongFilename.txt', './files/properFilename.md', (err) => {
+            if (err) throw err;
+            console.log('File renamed!');
+        })
+
+    } catch (error) {
+        errorLog(error)
+    }
 };
 
 await rename();
