8e15d51e17745618c2c35148a5074ed0ef323f95
Jushkin <jushkin.saparmatov@gmail.com>
2023-04-20 12:28:40 +0500

    initial commit

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import fs from 'fs'
+import path from 'path'
+
 const create = async () => {
-    // Write your code here 
+    const filePath = path.join('files', 'fresh.txt');
+  try {
+    await fs.promises.access(filePath);
+    throw new Error('FS operation failed');
+  } catch (error) {
+    if (error.code !== 'ENOENT') {
+      throw error;
+    }
+    await fs.promises.writeFile(filePath, 'I am fresh and young');
+  }
 };
 
 await create();
