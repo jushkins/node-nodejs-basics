2bb3c59db455ac4a5d1c8dfe765ebaf7bfa06f0f
Jushkin <jushkin.saparmatov@gmail.com>
2023-04-22 16:02:35 +0500

    cli/args.js solved

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+    const rssVars = {name: 'hello', age: 17};
+    for (const [key, value] of Object.entries(process.env)) {
+        const [prefix, name] = key.split('_');
+        if (prefix === 'RSS' && name in rssVars) {
+          rssVars[name] = value;
+        }
+      }
+    
+      const rssStr = Object.entries(rssVars).map(([name, value]) => `RSS_${name}=${value}`).join("; ");
+      console.log(rssStr);
 };
 
 parseEnv();
