d7926fb2bfd2d9b5d975ce30216f040a4c0abccd
Jushkin <jushkin.saparmatov@gmail.com>
2023-04-24 11:09:40 +0500

    Modules section from basics solved

--- src/modules/cjsToEsm.cjs
+++ src/modules/cjsToEsm.cjs
@@ -1,16 +1,20 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path from 'path';
+import { release, version } from 'os';
+import { createServer as createServerHttp } from 'http';
+import { fileURLToPath } from 'node:url';
+import './files/c.js'
 
 const random = Math.random();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 let unknownObject;
-
+import a from './files/a.json'
+import b from './files/b.json'
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = a
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = b
 }
 
 console.log(`Release ${release()}`);
@@ -33,8 +37,5 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
-    unknownObject,
-    myServer,
-};
+export { unknownObject, myServer }
 
