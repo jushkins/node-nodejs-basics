dc11d319ef047b506490d44a41b0c7e36d265d0f
Jushkin <jushkin.saparmatov@gmail.com>
2023-04-24 15:05:25 +0500

    Zip, decompress from node_basics solved

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,15 @@
+import { createUnzip } from 'zlib'
+import { createReadStream, createWriteStream } from 'fs'
+import { pipeline } from 'stream';
+
 const decompress = async () => {
-    // Write your code here 
+    const unzip = createUnzip();
+    const input = createReadStream('./files/archive.gz');
+    const output = createWriteStream('./files/fileToCompress.txt');
+
+    pipeline(input, unzip, output, (err) => {
+        if (err) console.log(err);
+    })
 };
 
 await decompress();
