ee86063fa87b7c85453b0971a857929f18ef4e41
Jushkin <jushkin.saparmatov@gmail.com>
2023-04-24 16:51:17 +0500

    Child Processes from node_basics solved

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,18 @@
+import { spawn } from 'child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const childProcess = spawn('node', ['./files/script.js', ...args], { stdio: ['pipe', 'pipe', 'inherit'] });
+
+  process.stdin.pipe(childProcess.stdin);
+  childProcess.stdout.pipe(process.stdout);
+
+  process.stdin.on('end', () => {
+    childProcess.stdin.end();
+  });
+
+  childProcess.on('exit', (code, signal) => {
+    console.log(`Child process exited with code ${code} and signal ${signal}`);
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2', '3rd argument', '4th arguments']);
